f870f2bc3082b274245d0c5ba5df32ed0e02c6b1
Michael Richardson <mcr@sandelman.ca>
2011-11-19 10:10:58 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ravenhill::Application.config.secret_token = 'c189d377877ec1ffe9d199ec4206e217d26f2d028f1c07d80c20e78ba9b3b36d902b3d4287e1887cf2114e474d6b9ae1ce063720780b3eeed8845fb6fc5d62f7'
