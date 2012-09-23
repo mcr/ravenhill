86f8260d06921ab39e3f1fae55de481266d9c356
Michael Richardson <mcr@sandelman.ca>
2012-09-22 20:36:41 -0400

    welcome route belongs in guardians. move devise_for earlier

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Ravenhill::Application.routes.draw do
-  resources :guardians do
-    as_routes
-  end
-
   devise_for :guardians
 
-  resources :students do
+  resources :guardians do
     as_routes
     collection do
       get 'welcome'
     end
+  end
+
+  resources :students do
+    as_routes
   end
 
   root :to => "guardians#welcome"
