3bcaedd349a786c07cbfb75b400899ab1c5fee7f
Michael Richardson <mcr@sandelman.ca>
2012-09-23 16:44:13 -0400

    more sophisticated ability: let parents edit themselves and their children

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,9 +1,23 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user, format)
-    can :read,   :all
-    can :update, :all
-    can :create, :all
+  def initialize(guardian, format)
+    guardian ||= Guardian.new
+
+    Rails.logger.debug "Access ability setup for #{guardian.email}, format=#{format}"
+
+    if guardian.admin?
+      can :read,   :all
+      can :update, :all
+      can :create, :all
+    else
+      can :read,   Guardian, :id => guardian.id
+      can :update, Guardian, :id => guardian.id
+      can [:read, :update],   Student do |student|
+	student.guardians.inject(false) { |result, guard|
+	  result ||= (guard.id == guardian.id)
+	}
+      end
+    end
   end
 end
